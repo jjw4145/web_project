eec9f9f3dbd22e775ad252719a39934ddc597ad5
jjw4145 <jjw4145@naver.com>
2019-06-18 15:08:22 +0900

    first term project

---
+++ web_project/TermProject_final.js
@@ -0,0 +1,27 @@
+//TermProject_final_main.html
+
+// 1. 대여한 도서 목록을 나타내는 div에 php로 DB에 저장된 목록을 가져온다
+
+borrowButton.onclick = function(){
+
+} // 버튼을 누르면 대여 화면을 나타낸다
+
+// TermProject_final_borrow.html
+
+classify.onclick = function(){
+  
+} // 버튼을 누르면 여러가지 카테고리들에 대한 책의 목록을 나타낸다
+
+search.onclick = function(){
+
+} // 검색 버튼을 누르면 검색이 가능하고 도서를 사전식으로 나열해서 보여준다
+
+book_name.onclick = function(){
+  modalBoxInnerBorrowButton.onclick = function(){ // 모달 박스에 있는 대여 버튼
+
+  } // 대여 버튼을 누르면 2주 간 빌릴 수 있고 반납일자를 명시해줌
+
+  modalBoxInnerCancelButton.onclick = function(){
+
+  } // 취소 버튼을 누르면 모달 박스를 종료함
+} // 대여 하고 싶은 도서를 클릭하면 대여 절차를 위한 모달 박스를 띄운다
